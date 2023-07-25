bded276a2e18a4b82e3525e4e4428564fd7d9d54
Barshon <barshonweb@gmail.com>
2023-07-25 14:44:53 +0600

    return book functionality

---
+++ return.js
@@ -0,0 +1,19 @@
+document.getElementById("return-btn").addEventListener("click",function(){
+    const returnField= document.getElementById("return-input");
+    const returnValueString=returnField.value;
+    const returnValue=parseInt(returnValueString);
+    const returnShow=document.getElementById("return-output");
+    const previousReturnValueString= returnShow.innerText;
+    const previousReturnValue=parseInt(previousReturnValueString);
+    const newReturnValue= returnValue+previousReturnValue;
+    returnShow.innerText=newReturnValue;
+    returnField.value='';
+
+    const totalBooksStringElement=document.getElementById("total-output");
+    const totalBooksString=totalBooksStringElement.innerText;
+    const totalBooks=parseInt(totalBooksString);
+
+    const newTotal=totalBooks+returnValue;
+    totalBooksStringElement.innerText=newTotal;
+
+})
